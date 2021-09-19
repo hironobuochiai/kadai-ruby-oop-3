80d24567492c22e7e977c516031ee682d433e662
hironobu.ochiai <hironobuochiai113@gmail.com>
2021-09-19 13:08:05 +0000

    add kadai-human

---
+++ human.rb
@@ -0,0 +1,14 @@
+require './animal'
+require './thinkable'
+
+class Human < Animal
+  attr_accessor :name, :age, :hobby
+  def initialize(name, age, hobby)
+    self.name = name
+    self.age = age
+    self.hobby = hobby
+  end
+    include Thinkable
+end
+
+  
